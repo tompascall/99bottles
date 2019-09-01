89101703f9e4ef59795bac8a47088ba18abc6669
Gulacsy-Toth <tompascall@gmail.com>
2019-09-01 22:09:45 +0200

    add quantity method

--- lib/99bottles.d.ts
+++ lib/99bottles.d.ts
@@ -1,9 +1,11 @@
 export declare type Pronoun = 'one' | 'it';
 export declare type Container = 'bottle' | 'bottles';
+export declare type Quantity = 'no more' | number;
 export declare class Bottles {
     verse(number: number): string;
     verses(from: number, to: number): string;
     container(number: number): Container;
     pronoun(number: number): Pronoun;
+    quantity(number: number): Quantity;
     song(): string;
 }
--- src/99bottles.ts
+++ src/99bottles.ts
@@ -4,18 +4,17 @@ export type Pronoun = 'one' | 'it';
 
 export type Container = 'bottle' | 'bottles';
 
+export type Quantity = 'no more' | number;
+
 export class Bottles {
   verse(number: number) {
     switch (number) {
       case 0:
         return `No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`
-      case 1:
-        return `${number} ${this.container(number)} of beer on the wall, ${number} ${this.container(number)} of beer.
-Take ${this.pronoun(number)} down and pass it around, no more bottles of beer on the wall.`
       default:
         return `${number} ${this.container(number)} of beer on the wall, ${number} ${this.container(number)} of beer.
-Take ${this.pronoun(number)} down and pass it around, ${number - 1} ${this.container(number - 1)} of beer on the wall.`
+Take ${this.pronoun(number)} down and pass it around, ${this.quantity(number - 1)} ${this.container(number - 1)} of beer on the wall.`
     }
   }
 
@@ -37,6 +36,13 @@ Take ${this.pronoun(number)} down and pass it around, ${number - 1} ${this.conta
       return 'it';
     }
     return 'one';
+  }
+
+  quantity(number: number): Quantity {
+    if (number === 0) {
+      return 'no more'
+    }
+    return number;
   }
 
   song() {
