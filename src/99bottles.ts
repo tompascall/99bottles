a861ed5d8e0d804843c34c6cbdb42da8626d7640
Gulacsy-Toth <tompascall@gmail.com>
2019-09-03 22:47:13 +0200

    cache bottleNumber

--- src/99bottles.ts
+++ src/99bottles.ts
@@ -54,33 +54,20 @@ class BottleNumber {
 
 export class Bottles {
   verse(number: number) {
-      return `${capitalize(this.quantity(number))} ${this.container(number)} of beer on the wall, ${this.quantity(number)} ${this.container(number)} of beer.
-${this.action(number)}, ${this.quantity(this.successor(number))} ${this.container(this.successor(number))} of beer on the wall.`
+    const bottleNumber = new BottleNumber(number);
+    const nextBottleNumber = new BottleNumber(bottleNumber.successor());
+
+    return `${
+      capitalize(bottleNumber.quantity())} ${bottleNumber.container()} of beer on the wall, ${
+        bottleNumber.quantity()} ${bottleNumber.container()} of beer.
+${bottleNumber.action()}, ${nextBottleNumber.quantity()} ${
+  nextBottleNumber.container()} of beer on the wall.`
   }
 
   verses(from: number, to: number) {
     return range(from, to - 1)
       .map(i => this.verse(i))
       .join('\n\n');
-  }
-  container(number: number): Container {
-    return new BottleNumber(number).container();
-  }
-
-  pronoun(number: number): Pronoun {
-    return new BottleNumber(number).pronoun();
-  }
-
-  quantity(number: number): Quantity {
-    return new BottleNumber(number).quantity();
-  }
-
-  action(number: number): string {
-    return new BottleNumber(number).action();
-  }
-
-  successor(number: number): number {
-    return new BottleNumber(number).successor();
   }
 
   song() {
